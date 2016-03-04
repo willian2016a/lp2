9500d7629b43a6b9ea2ca435a031de9789cf9f78
willian2016a <willianalvesalmeida@yahoo.com.br>
2016-03-04 10:52:45 -0300

    Update Exercicio1_3.java

--- exercicio1/Exercicio1_3.java
+++ exercicio1/Exercicio1_3.java
@@ -1,16 +1,33 @@
 public class Exercicio1_3{
-  public static void main(String [] args){
-    int a = 45;
+   int a = 45;
     int b = 20;
     int c = 3;
     int x = 0;
+    
+  public void soma(){
     x = a+b+c;
     System.out.println("Valor da soma : " + x);
+  }
+  public void multiplicacao(){
     x = a*b*c;
-    System.out.println("Valor da multiplicação : " + x);
+     System.out.println("Valor da multiplicação : " + x);
+  }
+  public void subtracao(){
     x = a-b-c;
     System.out.println("Valor da subtração : " + x);
-    x = a/b/c;
-    System.out.println("Valor da divisão : " + x);
+  }
+  public void divisao(){
+     x = a/b/c;
+     System.out.println("Valor da divisão : " + x);
+  }
+
+
+
+  public static void main(String [] args){
+    Exercicio1_3 obj = new Exercicio1_3();
+    obj.soma();
+    obj.multiplicacao();
+    obj.subtracao();
+    obj.divisao();
   }
 }
