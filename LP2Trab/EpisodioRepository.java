456b83d5e30e3d100e156817c805c4dc0f16809d
willian2016a <willianalvesalmeida@yahoo.com.br>
2016-06-03 14:28:03 -0300

    Create EpisodioRepository.java

---
+++ LP2Trab/EpisodioRepository.java
@@ -0,0 +1,14 @@
+package br.com.herbertrausch.spring.mongo;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EpisodioRepository  extends MongoRepository<Cliente, String> {
+
+	List<Usuario>findByNomeEpisodio(String nomeEpisodio);
+	List<Usuario>findByIdTemporada(int idTemporada);
+
+}
