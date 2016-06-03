de2c0c047ac2c3a37584b2594393e3b46b788317
willian2016a <willianalvesalmeida@yahoo.com.br>
2016-06-03 16:57:10 -0300

    Update EpisodioRepository.java

--- LP2Trab/EpisodioRepository.java
+++ LP2Trab/EpisodioRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EpisodioRepository  extends MongoRepository<Cliente, String> {
+public interface EpisodioRepository  extends MongoRepository<Usuario, String> {
 
 	List<Usuario>findByNomeEpisodio(String nomeEpisodio);
 	List<Usuario>findByIdTemporada(int idTemporada);
